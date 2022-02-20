670eee2abe96ef4c20a7e16ebd3cf7e14c776c2a
Janith Chanaka Abhayarathna <abhayaratnajanith3@gmail.com>
2022-02-20 20:39:39 +0530

    Completed:sparkles:.

--- app/src/main/java/com/example/tute01_1/MainActivity.kt
+++ app/src/main/java/com/example/tute01_1/MainActivity.kt
@@ -7,21 +7,27 @@ import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.lang.Exception
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val res = mutableListOf<Int>()
+
+    val gen: Random = Random()
+    var new_number = 1 + gen.nextInt(49)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.bt)
 
-        button.setOnClickListener {
-            calculate()
-        }
-    }
-
-    fun calculate() {
+        val button0 = findViewById<Button>(R.id.btn1)
+        val button1 = findViewById<Button>(R.id.btn2)
+        val button2 = findViewById<Button>(R.id.btn3)
+        val button3 = findViewById<Button>(R.id.btn4)
+        val button4 = findViewById<Button>(R.id.btn5)
+        val button5 = findViewById<Button>(R.id.btn6)
 
         val result0 = findViewById<TextView>(R.id.result0)
         val result1 = findViewById<TextView>(R.id.result1)
@@ -30,13 +36,57 @@ class MainActivity : AppCompatActivity() {
         val result4 = findViewById<TextView>(R.id.result4)
         val result5 = findViewById<TextView>(R.id.result5)
 
-        val res = mutableListOf<Int>()
+        button.setOnClickListener {
+            calculate(result0,result1,result2,result3,result4,result5)
+        }
 
-        val gen: Random = Random()
-        var new_number = 1 + gen.nextInt(49)
+        button0.setOnClickListener {
+            val temp = genNumber()
+            result0.setText(temp.toString())
+            res[0] = temp
+        }
+        button1.setOnClickListener {
+            val temp = genNumber()
+            result1.setText(temp.toString())
+            res[1] = temp
+        }
+        button2.setOnClickListener {
+            val temp = genNumber()
+            result2.setText(temp.toString())
+            res[2] = temp
+        }
+        button3.setOnClickListener {
+            val temp = genNumber()
+            result3.setText(temp.toString())
+            res[3] = temp
+        }
+        button4.setOnClickListener {
+            val temp = genNumber()
+            result4.setText(temp.toString())
+            res[4] = temp
+        }
+        button5.setOnClickListener {
+            val temp = genNumber()
+            result5.setText(temp.toString())
+            res[5] = temp
+        }
+    }
 
+    fun calculate(result0: TextView,result1: TextView,result2: TextView,result3: TextView,result4: TextView,result5: TextView) {
+
+        var userInput = 0
+
+        val editText = findViewById<EditText>(R.id.change)
+
+        try {
+            userInput = editText.text.toString().toInt()
+        } catch (e: Exception) {
+
+        }
+
+        res.clear()
         while (res.size < 6) {
-            if (new_number !in res)
+            if (new_number !in res && new_number != userInput)
                 res.add(new_number)
             else
                 new_number = 1 + gen.nextInt(49)
@@ -48,10 +98,18 @@ class MainActivity : AppCompatActivity() {
         result3.setText(res[3].toString())
         result4.setText(res[4].toString())
         result5.setText(res[5].toString())
-
-        var editText = findViewById<EditText>(R.id.change)
-
-        var input = editText.text.toString()
         
+    }
+
+    fun genNumber(): Int {
+
+        var num = gen.nextInt(49)
+        if (num !in res) {
+            return num
+        } else {
+            var num = gen.nextInt(49)
+            return num
+        }
+
     }
 }
